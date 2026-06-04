37c5d35e31e3a62d91ad3f22c82a2677113452ce
Pradip Chaudhary <chaudharypradip678@gmail.com>
2026-06-04 23:33:21 +0545

    create about page

---
+++ app/about/page.tsx
@@ -0,0 +1,22 @@
+
+const AboutPage = () => {
+  return (
+    <div>
+      {/* Header Section */}
+      <section className="text-center max-w-3xl mx-auto pt-10">
+        <h1 className="text-4xl md:text-5xl font-bold text-gray-900">
+          About
+        </h1>
+
+        <p className="mt-4 text-gray-600 text-sm md:text-base">
+          This is the about page of our Next.js application.
+        </p>
+
+
+      </section>
+
+    </div>
+  )
+}
+
+export default AboutPage
